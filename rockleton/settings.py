eb393c60be13cd760d15994c81a8fe213206a6e9
Ed Rogers <edmund.rogers@gmail.com>
2016-01-01 14:35:16 -0800

    Second Commit

--- rockleton/settings.py
+++ rockleton/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'rockleton.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rockleton',
+        'USER': 'root',
+        'PASSWORD': 'Ciaran$66',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
+
+
 
 
 # Password validation
